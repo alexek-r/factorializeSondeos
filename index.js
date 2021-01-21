c221d534de6955f8d02828fe9deb6268203cc86a
arodriguez <alexis.rodriguez@sondeos.com.ar>
2021-01-21 16:37:36 -0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const factorialize = function(num){
 
     /* YOUR CODE HERE */
+    let i;
+    let total= num;
+
+    for(i = num-1 ; i>0; i--){
+        total *= i
+    }
+
+    return total
 
 }
 
